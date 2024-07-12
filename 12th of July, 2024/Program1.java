3cbe2f0c03685634a02e372b9bdbaa814dd5968e
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-07-12 14:05:42 +0530

    12th of july

---
+++ 12th of July, 2024/Program1.java
@@ -0,0 +1,11 @@
+class Program1{
+  public static void main(String[] args) {
+    int i = 10;
+
+    if (i < 9)
+    System.out.println("inside if block");
+
+    System.out.println("10 is less than 15");
+    System.out.println("I am not in if");
+  }
+}
