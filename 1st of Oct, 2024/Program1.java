d701a57b5da95480f9e96f6cbb1a3aa44f36fcba
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-10-02 22:05:21 +0530

    latest

---
+++ 1st of Oct, 2024/Program1.java
@@ -0,0 +1,18 @@
+// Write a Java program which creates a thread by using the following class:
+// Thread(Runnable r, String name)
+
+import java.io.*;
+import java.util.*;
+
+Thread(Runnable r, String name)
+
+public class Program1 implements Runnable {
+public void num() {
+  System.out.println(" Thread is created");
+  public static void main (String args[]) {
+      Runnable R1 = new Example ();
+      Thread t1 = new Thread(R1, "My Thread");
+      t1.start();
+    }
+  }
+}
